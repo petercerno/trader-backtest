2beaf83880938f0c2611d2d88ac4fd7422436fc6
Peter Cerno <petercerno@gmail.com>
2019-09-21 11:24:08 +0200

    Small refactoring

--- csv_convert_main.cc
+++ csv_convert_main.cc
@@ -29,7 +29,7 @@ DEFINE_string(start_date_utc, "", "Start date YYYY-MM-DD in UTC (included).");
 DEFINE_string(end_date_utc, "", "End date YYYY-MM-DD in UTC (excluded).");
 
 // Output compression.
-DEFINE_bool(compress, true, "Whether to compress the output file.");
+DEFINE_bool(compress, true, "Whether to compress the output protobuf file.");
 
 using namespace trader;
 
@@ -42,21 +42,19 @@ std::string DurationToString(long duration_sec) {
   return ss.str();
 }
 
-// Converts the price history csv file if enabled by flags.
-void ConvertPriceHistoryIfPossible(long start_timestamp_sec,
-                                   long end_timestamp_sec) {
-  const std::string& input_file = FLAGS_input_price_history_csv_file;
-  const std::string& output_file =
-      FLAGS_output_price_history_delimited_proto_file;
-  if (input_file.empty() || output_file.empty()) {
-    return;
-  }
+// Converts the price history CSV file to a delimited protobuf file.
+void ConvertPriceHistory(
+    const std::string& input_price_history_csv_file,
+    const std::string& output_price_history_delimited_proto_file,
+    long start_timestamp_sec, long end_timestamp_sec) {
   PriceHistory price_history;
   auto start = std::chrono::high_resolution_clock::now();
-  std::cout << "Reading price history CSV file: " << input_file << std::endl;
-  if (!ReadPriceHistoryFromCsvFile(input_file, &price_history)) {
-    std::cerr << "Failed to read price history CSV file: " << input_file
-              << std::endl;
+  std::cout << "Reading price history CSV file: "
+            << input_price_history_csv_file << std::endl;
+  if (!ReadPriceHistoryFromCsvFile(input_price_history_csv_file,
+                                   &price_history)) {
+    std::cerr << "Failed to read price history CSV file: "
+              << input_price_history_csv_file << std::endl;
     return;
   }
   if (!CheckPriceHistoryTimestamps(price_history)) {
@@ -76,12 +74,13 @@ void ConvertPriceHistoryIfPossible(long start_timestamp_sec,
   }
   const auto price_history_subset =
       HistorySubset(price_history, start_timestamp_sec, end_timestamp_sec);
-  std::cerr << "Writing price history file: " << output_file << std::endl;
-  if (!WriteDelimitedMessagesToFile(price_history_subset.first,
-                                    price_history_subset.second, output_file,
-                                    FLAGS_compress)) {
-    std::cerr << "Failed to write price history file: " << output_file
-              << std::endl;
+  std::cerr << "Writing price history file: "
+            << output_price_history_delimited_proto_file << std::endl;
+  if (!WriteDelimitedMessagesToFile(
+          price_history_subset.first, price_history_subset.second,
+          output_price_history_delimited_proto_file, FLAGS_compress)) {
+    std::cerr << "Failed to write price history file: "
+              << output_price_history_delimited_proto_file << std::endl;
     return;
   }
   auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(
@@ -90,30 +89,27 @@ void ConvertPriceHistoryIfPossible(long start_timestamp_sec,
             << duration.count() / 1000.0 << " seconds" << std::endl;
 }
 
-// Converts the OHLC history csv file if enabled by flags.
-void ConvertOhlcHistoryIfPossible(long start_timestamp_sec,
-                                  long end_timestamp_sec) {
-  const std::string& input_file = FLAGS_input_ohlc_history_csv_file;
-  const std::string& output_file =
-      FLAGS_output_ohlc_history_delimited_proto_file;
-  if (input_file.empty() || output_file.empty()) {
-    return;
-  }
+// Converts the OHLC history CSV file to delimited protobuf file.
+void ConvertOhlcHistory(
+    const std::string& input_ohlc_history_csv_file,
+    const std::string& output_ohlc_history_delimited_proto_file,
+    long start_timestamp_sec, long end_timestamp_sec) {
   OhlcHistory ohlc_history;
   auto start = std::chrono::high_resolution_clock::now();
-  std::cout << "Reading OHLC history CSV file: " << input_file << std::endl;
-  if (!ReadOhlcHistoryFromCsvFile(input_file, &ohlc_history)) {
-    std::cerr << "Failed to read OHLC history CSV file: " << input_file
-              << std::endl;
+  std::cout << "Reading OHLC history CSV file: " << input_ohlc_history_csv_file
+            << std::endl;
+  if (!ReadOhlcHistoryFromCsvFile(input_ohlc_history_csv_file, &ohlc_history)) {
+    std::cerr << "Failed to read OHLC history CSV file: "
+              << input_ohlc_history_csv_file << std::endl;
     return;
   }
   const auto ohlc_history_subset =
       HistorySubset(ohlc_history, start_timestamp_sec, end_timestamp_sec);
-  if (!WriteDelimitedMessagesToFile(ohlc_history_subset.first,
-                                    ohlc_history_subset.second, output_file,
-                                    FLAGS_compress)) {
-    std::cerr << "Failed to write OHLC history file: " << output_file
-              << std::endl;
+  if (!WriteDelimitedMessagesToFile(
+          ohlc_history_subset.first, ohlc_history_subset.second,
+          output_ohlc_history_delimited_proto_file, FLAGS_compress)) {
+    std::cerr << "Failed to write OHLC history file: "
+              << output_ohlc_history_delimited_proto_file << std::endl;
     return;
   }
   auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(
@@ -132,14 +128,28 @@ int main(int argc, char* argv[]) {
   long start_timestamp_sec = 0;
   long end_timestamp_sec = 0;
   if (!ConvertDateUTCToTimestampSec(FLAGS_start_date_utc,
-                                    &start_timestamp_sec) ||
-      !ConvertDateUTCToTimestampSec(FLAGS_end_date_utc, &end_timestamp_sec)) {
-    std::cerr << "Invalid time period" << std::endl;
+                                    &start_timestamp_sec)) {
+    std::cerr << "Invalid start date: " << FLAGS_start_date_utc << std::endl;
+    return 1;
+  }
+  if (!ConvertDateUTCToTimestampSec(FLAGS_end_date_utc, &end_timestamp_sec)) {
+    std::cerr << "Invalid end date: " << FLAGS_end_date_utc << std::endl;
     return 1;
   }
 
-  ConvertPriceHistoryIfPossible(start_timestamp_sec, end_timestamp_sec);
-  ConvertOhlcHistoryIfPossible(start_timestamp_sec, end_timestamp_sec);
+  if (!FLAGS_input_price_history_csv_file.empty() &&
+      !FLAGS_output_price_history_delimited_proto_file.empty()) {
+    ConvertPriceHistory(FLAGS_input_price_history_csv_file,
+                        FLAGS_output_price_history_delimited_proto_file,
+                        start_timestamp_sec, end_timestamp_sec);
+  }
+
+  if (!FLAGS_input_ohlc_history_csv_file.empty() &&
+      !FLAGS_output_ohlc_history_delimited_proto_file.empty()) {
+    ConvertOhlcHistory(FLAGS_input_ohlc_history_csv_file,
+                       FLAGS_output_ohlc_history_delimited_proto_file,
+                       start_timestamp_sec, end_timestamp_sec);
+  }
 
   // Optional: Delete all global objects allocated by libprotobuf.
   google::protobuf::ShutdownProtobufLibrary();
