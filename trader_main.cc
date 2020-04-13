cb07aa279a0b0fee6a5c8027c518c84d329e361c
Peter Cerno <petercerno@gmail.com>
2020-04-13 21:31:17 +0200

    Fixing trader_main

--- trader_main.cc
+++ trader_main.cc
@@ -1,16 +1,8 @@
-// Copyright © 2019 Peter Cerno. All rights reserved.
-
-#include <chrono>
-#include <fstream>
-#include <iostream>
-#include <string>
-#include <utility>
-#include <vector>
+// Copyright © 2020 Peter Cerno. All rights reserved.
 
 #include <gflags/gflags.h>
 
 #include "limit_trader.h"
-#include "limit_trader_v2.h"
 #include "stop_trader.h"
 #include "trader_base.h"
 #include "trader_eval.h"
@@ -19,21 +11,17 @@
 #include "util_proto.h"
 #include "util_time.h"
 
-// Input/Output files.
 DEFINE_string(input_price_history_delimited_proto_file, "",
               "Input file containing the delimited PriceRecord protos.");
 DEFINE_string(input_ohlc_history_delimited_proto_file, "",
               "Input file containing the delimited OhlcRecord protos.");
-DEFINE_string(output_exchange_states_csv_file, "",
-              "Output CSV file containing the exchange account states.");
 
-// Trader.
-DEFINE_string(trader, "limit-v2",
-              "Trader to be executed. [limit, limit-v2, stop].");
+DEFINE_string(trader, "limit", "Trader to be executed. [limit, stop].");
+DEFINE_string(output_exchange_log_file, "",
+              "Output CSV file containing the exchange log.");
 DEFINE_string(output_trader_log_file, "",
               "Output file containing the trader-dependent log.");
 
-// Time period and sampling.
 DEFINE_string(start_date_utc, "2016-01-01",
               "Start date YYYY-MM-DD in UTC (included).");
 DEFINE_string(end_date_utc, "2017-01-01",
@@ -43,18 +31,13 @@ DEFINE_double(max_price_deviation_per_min, 0.05,
 DEFINE_int32(sampling_rate_sec, 300, "Sampling rate in seconds.");
 DEFINE_int32(evaluation_period_months, 6, "Evaluation period in months.");
 
-// Starting balance.
 DEFINE_double(start_security_balance, 1.0, "Starting security amount.");
 DEFINE_double(start_cash_balance, 0.0, "Starting cash amount.");
 
-// Parameters of the exchange account.
-DEFINE_double(order_execution_accuracy, 0.5,
-              "Accuracy of executing market / stop orders at their price.");
-DEFINE_double(limit_order_fill_volume_ratio, 0.5,
-              "Ratio of (tick) volume used to fill the limit order. "
-              "Not used if zero.");
-
-// Evaluate a batch o parameters.
+DEFINE_double(market_liquidity, 0.5,
+              "Liquidity for executing market (stop) orders.");
+DEFINE_double(max_volume_ratio, 0.5,
+              "Fraction of tick volume used to fill the limit order.");
 DEFINE_bool(evaluate_batch, false, "Batch evaluation.");
 
 using namespace trader;
@@ -62,102 +45,83 @@ using namespace trader;
 namespace {
 const char kStopTraderName[] = "stop";
 const char kLimitTraderName[] = "limit";
-const char kLimitTraderV2Name[] = "limit-v2";
 
-// Returns the ExchangeAccountConfig based on the flags (and default values).
-ExchangeAccountConfig GetExchangeAccountConfig() {
-  ExchangeAccountConfig config;
+// Returns the TraderAccountConfig based on the flags (and default values).
+TraderAccountConfig GetTraderAccountConfig() {
+  TraderAccountConfig config;
   config.set_start_security_balance(FLAGS_start_security_balance);
   config.set_start_cash_balance(FLAGS_start_cash_balance);
-  config.set_security_balance_unit(0.01f);
-  config.set_cash_balance_unit(0.01f);
-  config.mutable_market_order_fee_config()->set_relative_fee(0.0025f);
+  config.set_security_unit(0.01f);
+  config.set_cash_unit(0.01f);
+  config.mutable_market_order_fee_config()->set_relative_fee(0.005f);
   config.mutable_market_order_fee_config()->set_fixed_fee(0);
   config.mutable_market_order_fee_config()->set_minimum_fee(0);
-  config.mutable_limit_order_fee_config()->set_relative_fee(0);
+  config.mutable_limit_order_fee_config()->set_relative_fee(0.005);
   config.mutable_limit_order_fee_config()->set_fixed_fee(0);
   config.mutable_limit_order_fee_config()->set_minimum_fee(0);
-  config.mutable_stop_order_fee_config()->set_relative_fee(0.0025f);
+  config.mutable_stop_order_fee_config()->set_relative_fee(0.005f);
   config.mutable_stop_order_fee_config()->set_fixed_fee(0);
   config.mutable_stop_order_fee_config()->set_minimum_fee(0);
-  config.set_market_order_accuracy(FLAGS_order_execution_accuracy);
-  config.set_stop_order_accuracy(FLAGS_order_execution_accuracy);
-  config.set_limit_order_fill_volume_ratio(FLAGS_limit_order_fill_volume_ratio);
+  config.set_market_liquidity(FLAGS_market_liquidity);
+  config.set_max_volume_ratio(FLAGS_max_volume_ratio);
   return config;
 }
 
-// Returns the default stop trader instance.
-TraderInstance GetStopTraderInstance() {
+// Returns the default stop trader factory.
+std::unique_ptr<TraderFactoryInterface> GetDefaultStopTraderFactory() {
   StopTraderConfig config;
   config.set_stop_order_margin(0.2f);
   config.set_stop_order_move_margin(0.1f);
   config.set_stop_order_increase_per_day(0.2f);
   config.set_stop_order_decrease_per_day(0.2f);
-  return TraderInstance(new StopTrader(config));
+  return std::unique_ptr<TraderFactoryInterface>(new StopTraderFactory(config));
 }
 
 // Returns the default batch of stop traders.
-TraderBatch GetStopTraderBatch() {
-  return StopTrader::GetBatch(
+std::vector<std::unique_ptr<TraderFactoryInterface>> GetBatchOfStopTraders() {
+  return StopTraderFactory::GetBatchOfTraders(
       /* stop_order_margins = */ {0.15, 0.2, 0.25, 0.3},
       /* stop_order_move_margins = */ {0.05, 0.08, 0.1, 0.12},
       /* stop_order_increases_per_day = */ {0.1, 0.15, 0.2, 0.25},
       /* stop_order_decreases_per_day = */ {0.1, 0.15, 0.2, 0.25});
 }
 
-// Returns the default limit trader instance.
-TraderInstance GetLimitTraderInstance() {
+// Returns the default limit trader factory.
+std::unique_ptr<TraderFactoryInterface> GetDefaultLimitTraderFactory() {
   LimitTraderConfig config;
-  config.set_alpha_per_hour(0.03f);
-  config.set_limit_order_margin(0.01f);
-  return TraderInstance(new LimitTrader(config));
-}
-
-// Returns the default batch of limit traders.
-TraderBatch GetLimitTraderBatch() {
-  return LimitTrader::GetBatch(
-      /* alphas_per_hour = */ {0.01f, 0.02f, 0.03f},
-      /* limit_order_margins = */ {0.005f, 0.01f, 0.015f});
-}
-
-// Returns the default limit trader instance.
-TraderInstance GetLimitTraderV2Instance() {
-  LimitTraderV2Config config;
   config.set_alpha_per_hour(0.03f);
   config.set_limit_buy_margin(0.01f);
   config.set_limit_sell_margin(0.01f);
-  return TraderInstance(new LimitTraderV2(config));
+  return std::unique_ptr<TraderFactoryInterface>(
+      new LimitTraderFactory(config));
 }
 
 // Returns the default batch of limit traders.
-TraderBatch GetLimitTraderV2Batch() {
-  return LimitTraderV2::GetBatch(
+std::vector<std::unique_ptr<TraderFactoryInterface>> GetBatchOfLimitTraders() {
+  return LimitTraderFactory::GetBatchOfTraders(
       /* alphas_per_hour = */ {0.01f, 0.02f, 0.03f},
       /* limit_buy_margins = */ {0.005f, 0.01f, 0.015f},
       /* limit_sell_margins = */ {0.005f, 0.01f, 0.015f});
 }
 
-// Returns the default trader instance.
-TraderInstance GetTraderInstance() {
+// Returns the default trader factory.
+std::unique_ptr<TraderFactoryInterface> GetDefaultTraderFactory() {
   if (FLAGS_trader == kStopTraderName) {
-    return GetStopTraderInstance();
-  } else if (FLAGS_trader == kLimitTraderName) {
-    return GetLimitTraderInstance();
+    return GetDefaultStopTraderFactory();
   } else {
-    assert(FLAGS_trader == kLimitTraderV2Name);  // Invalid trader.
-    return GetLimitTraderV2Instance();
+    assert(FLAGS_trader == kLimitTraderName);
+    return GetDefaultLimitTraderFactory();
   }
 }
 
 // Returns the default batch of traders.
-TraderBatch GetTraderBatch() {
+std::vector<std::unique_ptr<TraderFactoryInterface>>
+GetDefaultBatchOfTraders() {
   if (FLAGS_trader == kStopTraderName) {
-    return GetStopTraderBatch();
-  } else if (FLAGS_trader == kLimitTraderName) {
-    return GetLimitTraderBatch();
+    return GetBatchOfStopTraders();
   } else {
-    assert(FLAGS_trader == kLimitTraderV2Name);  // Invalid trader.
-    return GetLimitTraderV2Batch();
+    assert(FLAGS_trader == kLimitTraderName);
+    return GetBatchOfLimitTraders();
   }
 }
 
@@ -233,6 +197,25 @@ OhlcHistory GetOhlcHistoryFromFlags(long start_timestamp_sec,
   }
   return {};
 }
+
+// Opens the file for logging purposes.
+std::unique_ptr<std::ofstream> OpenLogFile(const std::string& log_filename) {
+  if (log_filename.empty()) {
+    return nullptr;
+  }
+  if (FLAGS_evaluation_period_months > 0) {
+    std::cerr << "Logging disabled when evaluating multiple periods"
+              << std::endl;
+    return nullptr;
+  }
+  auto log_stream = std::unique_ptr<std::ofstream>(new std::ofstream());
+  log_stream->open(log_filename, std::ios::out | std::ios::trunc);
+  if (!log_stream->is_open()) {
+    std::cerr << "Cannot open file: " << log_filename << std::endl;
+    return nullptr;
+  }
+  return log_stream;
+}
 }  // namespace
 
 int main(int argc, char* argv[]) {
@@ -242,7 +225,7 @@ int main(int argc, char* argv[]) {
 
   gflags::ParseCommandLineFlags(&argc, &argv, true);
 
-  ExchangeAccountConfig exchange_account_config = GetExchangeAccountConfig();
+  TraderAccountConfig trader_account_config = GetTraderAccountConfig();
 
   long start_timestamp_sec = 0;
   long end_timestamp_sec = 0;
@@ -254,10 +237,12 @@ int main(int argc, char* argv[]) {
     return 1;
   }
 
-  EvalConfig eval_config;
-  eval_config.set_start_timestamp_sec(static_cast<int>(start_timestamp_sec));
-  eval_config.set_end_timestamp_sec(static_cast<int>(end_timestamp_sec));
-  eval_config.set_evaluation_period_months(FLAGS_evaluation_period_months);
+  TraderEvaluationConfig trader_eval_config;
+  trader_eval_config.set_start_timestamp_sec(
+      static_cast<int>(start_timestamp_sec));
+  trader_eval_config.set_end_timestamp_sec(static_cast<int>(end_timestamp_sec));
+  trader_eval_config.set_evaluation_period_months(
+      FLAGS_evaluation_period_months);
 
   OhlcHistory ohlc_history =
       GetOhlcHistoryFromFlags(start_timestamp_sec, end_timestamp_sec);
@@ -266,40 +251,33 @@ int main(int argc, char* argv[]) {
     return 1;
   }
 
-  TraderEval trader_eval(exchange_account_config, eval_config, ohlc_history);
-
   if (FLAGS_evaluate_batch) {
     std::cout << std::endl << "Batch evaluation:" << std::endl;
-    TraderBatch trader_batch = GetTraderBatch();
-    std::vector<EvalResult> eval_results =
-        trader_eval.EvaluateBatch(trader_batch);
-    std::sort(eval_results.begin(), eval_results.end(),
-              [](const EvalResult& lhs, const EvalResult& rhs) {
+    std::vector<std::unique_ptr<TraderFactoryInterface>> trader_factories =
+        GetDefaultBatchOfTraders();
+    std::vector<TraderEvaluationResult> trader_eval_results =
+        EvaluateBatchOfTraders(trader_account_config, trader_eval_config,
+                               ohlc_history, trader_factories);
+    std::sort(trader_eval_results.begin(), trader_eval_results.end(),
+              [](const TraderEvaluationResult& lhs,
+                 const TraderEvaluationResult& rhs) {
                 return lhs.score() > rhs.score();
               });
-    PrintEvalResults(eval_results, 20);
+    PrintTraderEvalResults(trader_eval_results, 20);
   } else {
     std::cout << std::endl << "Trader evaluation:" << std::endl;
-    TraderInstance trader_instance = GetTraderInstance();
-    std::ofstream trader_log_stream;
-    if (!FLAGS_output_trader_log_file.empty()) {
-      if (FLAGS_evaluation_period_months > 0) {
-        std::cerr << "Logging disabled when evaluating multiple periods"
-                  << std::endl;
-      } else {
-        trader_log_stream.open(FLAGS_output_trader_log_file,
-                               std::ios::out | std::ios::trunc);
-        if (!trader_log_stream.is_open()) {
-          std::cerr << "Cannot open file: " << FLAGS_output_trader_log_file
-                    << std::endl;
-          return 1;
-        }
-        trader_instance->SetLogStream(&trader_log_stream);
-      }
-    }
-    EvalResult eval_result = trader_eval.Evaluate(
-        trader_instance.get(), /* keep_intermediate_states = */ true);
-    for (const EvalResult::Period& period : eval_result.period()) {
+    std::unique_ptr<TraderFactoryInterface> trader_factory =
+        GetDefaultTraderFactory();
+    std::unique_ptr<std::ofstream> exchange_log_stream =
+        OpenLogFile(FLAGS_output_exchange_log_file);
+    std::unique_ptr<std::ofstream> trader_log_stream =
+        OpenLogFile(FLAGS_output_trader_log_file);
+    TraderEvaluationResult trader_eval_result =
+        EvaluateTrader(trader_account_config, trader_eval_config, ohlc_history,
+                       *trader_factory.get(), exchange_log_stream.get(),
+                       trader_log_stream.get());
+    for (const TraderEvaluationResult::Period& period :
+         trader_eval_result.period()) {
       std::cout << "["
                 << ConvertTimestampSecToDateTimeUTC(
                        period.start_timestamp_sec())
@@ -308,20 +286,6 @@ int main(int argc, char* argv[]) {
                 << "): "
                 << period.trader_final_gain() / period.base_final_gain()
                 << std::endl;
-    }
-    if (eval_result.period_size() == 1 &&
-        eval_result.period(0).exchange_account_state_size() > 0 &&
-        !FLAGS_output_exchange_states_csv_file.empty()) {
-      ExchangeAccountStates exchange_account_states;
-      exchange_account_states.reserve(
-          eval_result.period(0).exchange_account_state_size());
-      for (const ExchangeAccountState& state :
-           eval_result.period(0).exchange_account_state()) {
-        exchange_account_states.push_back(state);
-      }
-      WriteExchangeAccountStatesToCsvFile(
-          exchange_account_states, FLAGS_output_exchange_states_csv_file);
-      PrintExchangeAccountStatesStats(exchange_account_states);
     }
   }
 
