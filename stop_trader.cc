ef6ad10ac9af78dcf90a049709184cad16b2139d
Peter Cerno <petercerno@gmail.com>
2020-04-13 15:36:01 +0200

    Updating stop trader

--- stop_trader.cc
+++ stop_trader.cc
@@ -7,86 +7,42 @@
 #include <sstream>
 
 namespace trader {
-namespace {
-// Number of seconds per day.
-static const int kSecondsPerDay = 24 * 60 * 60;
-}  // namespace
-
-StopTrader::StopTrader(const StopTraderConfig& trader_config)
-    : trader_config_(trader_config) {}
 
 void StopTrader::Update(const OhlcTick& ohlc_tick, float security_balance,
                         float cash_balance, std::vector<Order>* orders) {
-  assert(orders != nullptr);  // Output orders must be provided.
+  assert(orders != nullptr);
   const int timestamp_sec = ohlc_tick.timestamp_sec();
   const float price = ohlc_tick.close();
-  assert(timestamp_sec > timestamp_sec_);  // Invalid OHLC tick timestamp.
-  assert(price > 0);                       // Invalid OHLC tick close price.
-  assert(security_balance > 0 || cash_balance > 0);  // Invalid balance(s).
+  assert(timestamp_sec > last_timestamp_sec_);
+  assert(price > 0);
+  assert(security_balance > 0 || cash_balance > 0);
   const Mode mode = (security_balance * price >= cash_balance) ? Mode::IN_LONG
                                                                : Mode::IN_CASH;
-  if (timestamp_sec >= timestamp_sec_ + max_age_sec_ || mode != mode_) {
-    InitStopOrderPrice(mode, price);
-  } else if (timestamp_sec >= timestamp_sec_ + min_age_sec_) {
+  if (timestamp_sec >= last_timestamp_sec_ + max_allowed_gap_sec_ ||
+      mode != mode_) {
+    if (mode == Mode::IN_LONG) {
+      stop_order_price_ = (1 - trader_config_.stop_order_margin()) * price;
+    } else {
+      assert(mode == Mode::IN_CASH);
+      stop_order_price_ = (1 + trader_config_.stop_order_margin()) * price;
+    }
+  } else {
     assert(mode == mode_);
     UpdateStopOrderPrice(mode, timestamp_sec, price);
   }
-  timestamp_sec_ = timestamp_sec;
-  security_balance_ = security_balance;
-  cash_balance_ = cash_balance;
+  last_security_balance_ = security_balance;
+  last_cash_balance_ = cash_balance;
+  last_timestamp_sec_ = timestamp_sec;
+  last_close_ = price;
   mode_ = mode;
   EmitStopOrder(price, orders);
 }
 
-std::string StopTrader::ToString() const {
-  std::stringstream ss;
-  ss << std::fixed << std::setprecision(3) << "stop-trader ["
-     << trader_config_.stop_order_margin() << " "
-     << trader_config_.stop_order_move_margin() << " "
-     << trader_config_.stop_order_increase_per_day() << " "
-     << trader_config_.stop_order_decrease_per_day() << "]";
-  return ss.str();
-}
-
-TraderInstance StopTrader::NewInstance() const {
-  return TraderInstance(new StopTrader(trader_config_));
-}
-
-TraderBatch StopTrader::GetBatch(
-    const std::vector<float>& stop_order_margins,
-    const std::vector<float>& stop_order_move_margins,
-    const std::vector<float>& stop_order_increases_per_day,
-    const std::vector<float>& stop_order_decreases_per_day) {
-  TraderBatch batch;
-  for (float stop_order_margin : stop_order_margins)
-    for (float stop_order_move_margin : stop_order_move_margins)
-      for (float stop_order_increase_per_day : stop_order_increases_per_day)
-        for (float stop_order_decrease_per_day : stop_order_decreases_per_day) {
-          StopTraderConfig trader_config;
-          trader_config.set_stop_order_margin(stop_order_margin);
-          trader_config.set_stop_order_move_margin(stop_order_move_margin);
-          trader_config.set_stop_order_increase_per_day(
-              stop_order_increase_per_day);
-          trader_config.set_stop_order_decrease_per_day(
-              stop_order_decrease_per_day);
-          batch.emplace_back(new StopTrader(trader_config));
-        }
-  return batch;
-}
-
-void StopTrader::InitStopOrderPrice(Mode mode, float price) {
-  if (mode == Mode::IN_LONG) {
-    stop_order_price_ = (1 - trader_config_.stop_order_margin()) * price;
-  } else {
-    assert(mode == Mode::IN_CASH);  // Invalid mode.
-    stop_order_price_ = (1 + trader_config_.stop_order_margin()) * price;
-  }
-}
-
 void StopTrader::UpdateStopOrderPrice(Mode mode, int timestamp_sec,
                                       float price) {
+  static constexpr int kSecondsPerDay = 24 * 60 * 60;
   const float sampling_rate_sec =
-      std::min(kSecondsPerDay, timestamp_sec - timestamp_sec_);
+      std::min(kSecondsPerDay, timestamp_sec - last_timestamp_sec_);
   const float ticks_per_day = kSecondsPerDay / sampling_rate_sec;
   if (mode == Mode::IN_LONG) {
     const float stop_order_increase_threshold =
@@ -100,7 +56,7 @@ void StopTrader::UpdateStopOrderPrice(Mode mode, int timestamp_sec,
           price, (1 + stop_order_increase_per_tick) * stop_order_price_);
     }
   } else {
-    assert(mode == Mode::IN_CASH);  // Invalid mode.
+    assert(mode == Mode::IN_CASH);
     const float stop_order_decrease_threshold =
         (1 - trader_config_.stop_order_move_margin()) * stop_order_price_;
     if (price < stop_order_decrease_threshold) {
@@ -115,36 +71,78 @@ void StopTrader::UpdateStopOrderPrice(Mode mode, int timestamp_sec,
 }
 
 void StopTrader::EmitStopOrder(float price, std::vector<Order>* orders) const {
-  if (LogStream() != nullptr) {
-    constexpr char kSeparator = ',';
-    std::ostream& os = *LogStream();
-    std::string mode;
-    if (mode_ == Mode::IN_LONG) {
-      mode = "IN_LONG";
-    } else {
-      assert(mode_ == Mode::IN_CASH);  // Invalid mode.
-      mode = "IN_CASH";
-    }
-    os << std::fixed << std::setprecision(2)
-       << timestamp_sec_ << kSeparator
-       << price << kSeparator
-       << security_balance_ << kSeparator
-       << cash_balance_ << kSeparator
-       << mode << kSeparator
-       << stop_order_price_ << std::endl;
-  }
   orders->emplace_back();
   Order* order = &orders->back();
   order->set_type(Order_Type_STOP);
-  order->set_price(stop_order_price_);
   if (mode_ == Mode::IN_LONG) {
     order->set_side(Order_Side_SELL);
-    order->set_security_amount(security_balance_);
+    order->set_security_amount(last_security_balance_);
   } else {
-    assert(mode_ == Mode::IN_CASH);  // Invalid mode.
+    assert(mode_ == Mode::IN_CASH);
     order->set_side(Order_Side_BUY);
-    order->set_cash_amount(cash_balance_);
+    order->set_cash_amount(last_cash_balance_);
   }
+  order->set_price(stop_order_price_);
+}
+
+void StopTrader::LogInternalState(std::ostream* os) const {
+  if (os == nullptr) {
+    return;
+  }
+  std::string mode;
+  if (mode_ == Mode::IN_LONG) {
+    mode = "IN_LONG";
+  } else {
+    assert(mode_ == Mode::IN_CASH);
+    mode = "IN_CASH";
+  }
+  *os << std::fixed << std::setprecision(0)  // nowrap
+      << last_timestamp_sec_ << ","          // nowrap
+      << std::setprecision(3)                // nowrap
+      << last_security_balance_ << ","       // nowrap
+      << last_cash_balance_ << ","           // nowrap
+      << last_close_ << ","                  // nowrap
+      << mode << ","                         // nowrap
+      << stop_order_price_ << std::endl;     // nowrap
+}
+
+std::string StopTraderFactory::GetTraderName() const {
+  std::stringstream ss;
+  ss << std::fixed << std::setprecision(3) << "stop-trader["
+     << trader_config_.stop_order_margin() << "|"
+     << trader_config_.stop_order_move_margin() << "|"
+     << trader_config_.stop_order_increase_per_day() << "|"
+     << trader_config_.stop_order_decrease_per_day() << "]";
+  return ss.str();
+}
+
+std::unique_ptr<TraderInterface> StopTraderFactory::NewTrader() const {
+  return std::unique_ptr<StopTrader>(new StopTrader(trader_config_));
+}
+
+std::vector<std::unique_ptr<TraderFactoryInterface>>
+StopTraderFactory::GetBatchOfTraders(
+    const std::vector<float>& stop_order_margins,
+    const std::vector<float>& stop_order_move_margins,
+    const std::vector<float>& stop_order_increases_per_day,
+    const std::vector<float>& stop_order_decreases_per_day) {
+  std::vector<std::unique_ptr<TraderFactoryInterface>> batch;
+  for (const float stop_order_margin : stop_order_margins)
+    for (const float stop_order_move_margin : stop_order_move_margins)
+      for (const float stop_order_increase_per_day :
+           stop_order_increases_per_day)
+        for (const float stop_order_decrease_per_day :
+             stop_order_decreases_per_day) {
+          StopTraderConfig trader_config;
+          trader_config.set_stop_order_margin(stop_order_margin);
+          trader_config.set_stop_order_move_margin(stop_order_move_margin);
+          trader_config.set_stop_order_increase_per_day(
+              stop_order_increase_per_day);
+          trader_config.set_stop_order_decrease_per_day(
+              stop_order_decrease_per_day);
+          batch.emplace_back(new StopTraderFactory(trader_config));
+        }
+  return batch;
 }
 
 }  // namespace trader
--- stop_trader.h
+++ stop_trader.h
@@ -1,4 +1,4 @@
-// Copyright © 2017 Peter Cerno. All rights reserved.
+// Copyright © 2020 Peter Cerno. All rights reserved.
 
 #ifndef STOP_TRADER_H
 #define STOP_TRADER_H
@@ -10,58 +10,63 @@ namespace trader {
 // Stop trader. Emits exactly one stop order per OHLC tick.
 class StopTrader : public TraderInterface {
  public:
-  explicit StopTrader(const StopTraderConfig& trader_config);
+  explicit StopTrader(const StopTraderConfig& trader_config)
+      : trader_config_(trader_config) {}
   virtual ~StopTrader() {}
 
-  // Updates the (internal) trader state and emits a new stop order.
-  virtual void Update(const OhlcTick& ohlc_tick, float security_balance,
-                      float cash_balance, std::vector<Order>* orders);
+  void Update(const OhlcTick& ohlc_tick, float security_balance,
+              float cash_balance, std::vector<Order>* orders) override;
+  void LogInternalState(std::ostream* os) const override;
 
-  // Returns a string representation of the trader (and its configuration).
-  virtual std::string ToString() const;
+ private:
+  // Enumeration of possible trader modes.
+  enum class Mode {
+    NONE,     // Undefined.
+    IN_LONG,  // Trader holds most of its assets in security (crypto currency).
+    IN_CASH   // Trader holds most of its assets in base currency.
+  };
 
-  // Returns a new (freshly initialized) instance of the same stop trader.
-  virtual TraderInstance NewInstance() const;
+  StopTraderConfig trader_config_;
+  // Last seen trader account balance.
+  float last_security_balance_ = 0.0f;
+  float last_cash_balance_ = 0.0f;
+  // Last seen UNIX timestamp (in seconds).
+  int last_timestamp_sec_ = 0;
+  // Last seen close price.
+  float last_close_ = 0.0f;
+  // Last trader mode.
+  Mode mode_ = Mode::NONE;
+  // Last security (crypto currency) price for stop orders.
+  float stop_order_price_ = 0;
 
-  // Returns a batch of stop traders (for all combinations of parameters).
-  static TraderBatch GetBatch(
+  // Maximum allowed timestamp gap (in seconds).
+  // When we encounter such gap, we re-initialize the trader.
+  int max_allowed_gap_sec_ = 10 * 24 * 60 * 60;  // 10 days.
+
+  // Updates the trader stop order price.
+  void UpdateStopOrderPrice(Mode mode, int timestamp_sec, float price);
+  // Emits the stop order based on the (updated) internal trader state.
+  void EmitStopOrder(float price, std::vector<Order>* orders) const;
+};
+
+// Factory that emits StopTraders.
+class StopTraderFactory : public TraderFactoryInterface {
+ public:
+  explicit StopTraderFactory(const StopTraderConfig& trader_config)
+      : trader_config_(trader_config) {}
+  virtual ~StopTraderFactory() {}
+
+  std::string GetTraderName() const override;
+  std::unique_ptr<TraderInterface> NewTrader() const override;
+
+  static std::vector<std::unique_ptr<TraderFactoryInterface>> GetBatchOfTraders(
       const std::vector<float>& stop_order_margins,
       const std::vector<float>& stop_order_move_margins,
       const std::vector<float>& stop_order_increases_per_day,
       const std::vector<float>& stop_order_decreases_per_day);
 
  private:
-  // Enumeration of possible trader modes.
-  enum class Mode {
-    NONE,     // Undefined.
-    IN_LONG,  // Trader holds all its assets in security (crypto currency).
-    IN_CASH   // Trader holds all its assets in base currency.
-  };
-
-  // Trader configuration.
   StopTraderConfig trader_config_;
-  // Last UNIX timestamp (in seconds) when the trader was updated.
-  int timestamp_sec_ = 0;
-  // Last security (crypto currency) balance.
-  float security_balance_ = 0;
-  // Last cash balance.
-  float cash_balance_ = 0;
-  // Last trader mode.
-  Mode mode_ = Mode::NONE;
-  // Last security (crypto currency) price for stop orders.
-  float stop_order_price_ = 0;
-
-  // Minimum required timestamp age (in seconds) for adjusting the stop price.
-  int min_age_sec_ = 30;  // 30 sec.
-  // Maximum allowed timestamp age (in seconds).
-  int max_age_sec_ = 10 * 24 * 60 * 60;  // 10 days.
-
-  // Initializes the trader stop order price.
-  void InitStopOrderPrice(Mode mode, float price);
-  // Updates the trader stop order price.
-  void UpdateStopOrderPrice(Mode mode, int timestamp_sec, float price);
-  // Emits the stop order based on the (updated) internal trader state.
-  void EmitStopOrder(float price, std::vector<Order>* orders) const;
 };
 
 }  // namespace trader
