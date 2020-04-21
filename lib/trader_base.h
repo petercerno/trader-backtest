a38c87135b199168e40f3f265a371d11dcd90da3
Peter Cerno <petercerno@gmail.com>
2020-04-21 08:40:07 +0200

    Updating comments

--- lib/trader_base.h
+++ lib/trader_base.h
@@ -32,22 +32,36 @@ using PriceHistory = std::vector<PriceRecord>;
 // Historical OHLC ticks over time.
 using OhlcHistory = std::vector<OhlcTick>;
 
-// Every trader works as follows:
-// - At every step the trader receives the latest OHLC tick T[i] and account
-//   balances. The current time is at the end of the OHLC tick T[i] time period.
-//   The trader updates its internal state based on this OHLC tick T[i].
-// - Then trader needs to decide what orders to emit. There are no active orders
-//   on the exchange at this moment (see the explanation below).
-// - Once the trader decides, which orders to emit, the exchange will execute
+// The trader execution model works as follows:
+// - At every step the trader receives the latest OHLC tick T[i], the current
+//   account balances, and updates its internal state. The current time is at
+//   the end of the OHLC tick `T[i]` time period. (Note that the trader is not
+//   updated on OHLC ticks with zero volume. Such OHLC ticks indicate a gap in
+//   a price history, which could have been caused by an unresponsive exchange
+//   or its API.)
+// - Then the trader needs to decide what orders to emit. There are no other
+//   active orders on the exchange at this moment (see the explanation below).
+// - Once the trader decides what orders to emit, the exchange will execute
 //   (or cancel) all these orders on the follow-up OHLC tick T[i+1]. The trader
-//   does not see the follow-up OHLC tick T[i+1], so the process is fair.
+//   does not see the follow-up OHLC tick T[i+1], so there is no peeking into
+//   the future.
 // - Once all orders are executed (or canceled) by the exchange, the trader
 //   receives the follow-up OHLC tick T[i+1] and the whole process repeats.
 // Note that at every step every order gets either executed or canceled by the
 // exchange. This is a design simplification so that there are no active orders
 // that the trader needs to maintain over time.
 // In practice, however, we would not cancel orders if they would be re-emitted
-// again. We would simply modify the existing orders if needed.
+// again. We would simply modify the existing orders (from the previous
+// iteration) based on the updated state.
+// Also note that the OHLC history sampling rate defines the frequency at which
+// the trader is updated and emits orders. Traders should be designed in
+// a frequency-agnostic way. In other words, they should have similar behavior
+// and performance regardless of how frequently they are called. Traders should
+// not assume anything about how often and when exactly they are called. One
+// reason for that is that the exchanges (or their APIs) sometimes become
+// unresponsive for random periods of time (and we see that e.g. in the gaps
+// in the historical price histories). Therefore, we encourage to test the
+// traders on OHLC histories with various sampling rates and gaps.
 class TraderInterface {
  public:
   TraderInterface() {}
