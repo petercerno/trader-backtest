7aa24f21a714f6ad60ad79071ddb7df6536f5dbe
Peter Cerno <petercerno@gmail.com>
2020-09-20 19:20:59 +0200

    Adding last_n_ohlc_ticks

---
+++ indicators/last_n_ohlc_ticks.cc
@@ -0,0 +1,84 @@
+// Copyright © 2020 Peter Cerno. All rights reserved.
+
+#include "indicators/last_n_ohlc_ticks.h"
+
+namespace trader {
+
+void LastNOhlcTicks::RegisterLastTickUpdatedCallback(
+    LastTickUpdatedCallback last_tick_updated_callback) {
+  last_tick_updated_callback_ = last_tick_updated_callback;
+}
+
+void LastNOhlcTicks::RegisterNewTickAddedCallback(
+    NewTickAddedCallback new_tick_added_callback) {
+  new_tick_added_callback_ = new_tick_added_callback;
+}
+
+void LastNOhlcTicks::RegisterNewTickAddedAndOldestTickRemovedCallback(
+    NewTickAddedAndOldestTickRemovedCallback
+        new_tick_added_and_oldest_tick_removed_callback) {
+  new_tick_added_and_oldest_tick_removed_callback_ =
+      new_tick_added_and_oldest_tick_removed_callback;
+}
+
+const std::deque<OhlcTick>& LastNOhlcTicks::GetLastNOhlcTicks() const {
+  return last_n_ohlc_ticks_;
+}
+
+void LastNOhlcTicks::Update(const OhlcTick& ohlc_tick) {
+  const int adjusted_timestamp_sec =
+      period_size_sec_ * (ohlc_tick.timestamp_sec() / period_size_sec_);
+  while (!last_n_ohlc_ticks_.empty() &&
+         last_n_ohlc_ticks_.back().timestamp_sec() + period_size_sec_ <
+             adjusted_timestamp_sec) {
+    const int prev_timestamp_sec = last_n_ohlc_ticks_.back().timestamp_sec();
+    const float prev_close = last_n_ohlc_ticks_.back().close();
+    last_n_ohlc_ticks_.emplace_back();
+    OhlcTick* top_ohlc_tick = &last_n_ohlc_ticks_.back();
+    top_ohlc_tick->set_timestamp_sec(prev_timestamp_sec + period_size_sec_);
+    top_ohlc_tick->set_open(prev_close);
+    top_ohlc_tick->set_high(prev_close);
+    top_ohlc_tick->set_low(prev_close);
+    top_ohlc_tick->set_close(prev_close);
+    top_ohlc_tick->set_volume(0);
+    NewOhlcTickAdded();
+  }
+  if (last_n_ohlc_ticks_.empty() ||
+      last_n_ohlc_ticks_.back().timestamp_sec() < adjusted_timestamp_sec) {
+    last_n_ohlc_ticks_.push_back(ohlc_tick);
+    last_n_ohlc_ticks_.back().set_timestamp_sec(adjusted_timestamp_sec);
+    NewOhlcTickAdded();
+  } else {
+    assert(last_n_ohlc_ticks_.back().timestamp_sec() == adjusted_timestamp_sec);
+    OhlcTick* top_ohlc_tick = &last_n_ohlc_ticks_.back();
+    const OhlcTick old_ohlc_tick_copy = *top_ohlc_tick;
+    top_ohlc_tick->set_high(std::max(top_ohlc_tick->high(), ohlc_tick.high()));
+    top_ohlc_tick->set_low(std::min(top_ohlc_tick->low(), ohlc_tick.low()));
+    top_ohlc_tick->set_close(ohlc_tick.close());
+    top_ohlc_tick->set_volume(top_ohlc_tick->volume() + ohlc_tick.volume());
+    LastOhlcTickUpdated(old_ohlc_tick_copy);
+  }
+}
+
+void LastNOhlcTicks::NewOhlcTickAdded() {
+  if (last_n_ohlc_ticks_.size() <= num_ohlc_ticks_) {
+    if (new_tick_added_callback_) {
+      new_tick_added_callback_(last_n_ohlc_ticks_.back());
+    }
+  } else {
+    const OhlcTick removed_ohlc_tick_copy = last_n_ohlc_ticks_.front();
+    last_n_ohlc_ticks_.pop_front();
+    if (new_tick_added_and_oldest_tick_removed_callback_) {
+      new_tick_added_and_oldest_tick_removed_callback_(
+          removed_ohlc_tick_copy, last_n_ohlc_ticks_.back());
+    }
+  }
+}
+
+void LastNOhlcTicks::LastOhlcTickUpdated(const OhlcTick& old_ohlc_tick) {
+  if (last_tick_updated_callback_) {
+    last_tick_updated_callback_(old_ohlc_tick, last_n_ohlc_ticks_.back());
+  }
+}
+
+}  // namespace trader
---
+++ indicators/last_n_ohlc_ticks.h
@@ -0,0 +1,86 @@
+// Copyright © 2020 Peter Cerno. All rights reserved.
+
+#ifndef INDICATORS_LAST_N_OHLC_TICKS_H
+#define INDICATORS_LAST_N_OHLC_TICKS_H
+
+#include "lib/trader_base.h"
+
+namespace trader {
+
+// Keeps track of the last N OHLC ticks with a specified period (in seconds).
+// We assume that this period is divisible by the period of update OHLC ticks.
+class LastNOhlcTicks {
+ public:
+  // Called after the last OHLC tick was updated, but no OHLC tick was added.
+  // This happens when the OHLC tick provided in the Update method (below)
+  // is fully contained in the period of the most recent OHLC tick in the deque.
+  // old_ohlc_tick: The previous OHLC tick in the deque that was updated.
+  // new_ohlc_tick: The updated (most recent) OHLC tick in the deque.
+  using LastTickUpdatedCallback = std::function<void(
+      const OhlcTick& old_ohlc_tick, const OhlcTick& new_ohlc_tick)>;
+  // Called after a new OHLC tick was added to the deque.
+  // This happens when the OHLC tick provided in the Update method (below)
+  // starts after the period of the most recent OHLC tick in the deque.
+  // new_ohlc_tick: The newly added (most recent) OHLC tick in the deque.
+  using NewTickAddedCallback =
+      std::function<void(const OhlcTick& new_ohlc_tick)>;
+  // Called after a new OHLC tick was added to the deque and the oldest OHLC
+  // tick was removed from the deque.
+  // This happens when the deque grows beyond N OHLC ticks, so we need to remove
+  // the oldest one.
+  // removed_ohlc_tick: The oldest OHLC tick that was removed from deque.
+  // new_ohlc_tick: The newly added (most recent) OHLC tick in the deque.
+  using NewTickAddedAndOldestTickRemovedCallback = std::function<void(
+      const OhlcTick& removed_ohlc_tick, const OhlcTick& new_ohlc_tick)>;
+
+  // Constructor.
+  // num_ohlc_ticks: Number N of OHLC ticks that we want to keep in the deque.
+  // period_size_sec: Period of the kept OHLC ticks (in seconds).
+  LastNOhlcTicks(int num_ohlc_ticks, int period_size_sec)
+      : num_ohlc_ticks_(num_ohlc_ticks), period_size_sec_(period_size_sec) {
+    assert(num_ohlc_ticks > 0);
+    assert(period_size_sec > 0);
+  }
+  virtual ~LastNOhlcTicks() {}
+
+  virtual void RegisterLastTickUpdatedCallback(
+      LastTickUpdatedCallback last_tick_updated_callback);
+  virtual void RegisterNewTickAddedCallback(
+      NewTickAddedCallback new_tick_added_callback);
+  virtual void RegisterNewTickAddedAndOldestTickRemovedCallback(
+      NewTickAddedAndOldestTickRemovedCallback
+          new_tick_added_and_oldest_tick_removed_callback);
+
+  // Returns the deque of last N OHLC ticks.
+  virtual const std::deque<OhlcTick>& GetLastNOhlcTicks() const;
+
+  // Updates the deque of last N OHLC ticks.
+  // We assume that period_size_sec_ is divisible by the period of ohlc_tick.
+  virtual void Update(const OhlcTick& ohlc_tick);
+
+ private:
+  int num_ohlc_ticks_ = 0;
+  int period_size_sec_ = 0;
+
+  std::deque<OhlcTick> last_n_ohlc_ticks_;
+
+  LastTickUpdatedCallback last_tick_updated_callback_;
+  NewTickAddedCallback new_tick_added_callback_;
+  NewTickAddedAndOldestTickRemovedCallback
+      new_tick_added_and_oldest_tick_removed_callback_;
+
+  void NewOhlcTickAdded();
+  void LastOhlcTickUpdated(const OhlcTick& old_ohlc_tick);
+};
+
+// Keeps track of the last N day OHLC ticks.
+class LastNDayOhlcTicks : public LastNOhlcTicks {
+  explicit LastNDayOhlcTicks(int num_days)
+      : LastNOhlcTicks{/*num_ohlc_ticks=*/num_days,
+                       /*period_size_sec=*/kSecondsPerDay} {}
+  virtual ~LastNDayOhlcTicks() {}
+};
+
+}  // namespace trader
+
+#endif  // INDICATORS_LAST_N_OHLC_TICKS_H
