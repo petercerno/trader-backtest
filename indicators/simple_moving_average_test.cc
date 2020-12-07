0e86ca06f41fd253c377118722d7bfd5380655dd
Peter Cerno <petercerno@gmail.com>
2020-12-07 18:30:35 +0100

    Adding indicators testing utils

--- indicators/simple_moving_average_test.cc
+++ indicators/simple_moving_average_test.cc
@@ -3,64 +3,14 @@
 #include "indicators/simple_moving_average.h"
 
 #include "gtest/gtest.h"
+#include "indicators/test_util.h"
 
 namespace trader {
-namespace {
-static constexpr int kSecondsPer8Hours = 8 * 60 * 60;
-
-// Adds OHLC tick to the history. Does not set the timestamp_sec.
-void AddOhlcTick(float open, float high, float low, float close,
-                 OhlcHistory* ohlc_history) {
-  ASSERT_LE(low, open);
-  ASSERT_LE(low, high);
-  ASSERT_LE(low, close);
-  ASSERT_GE(high, open);
-  ASSERT_GE(high, close);
-  ohlc_history->emplace_back();
-  OhlcTick* ohlc_tick = &ohlc_history->back();
-  ohlc_tick->set_open(open);
-  ohlc_tick->set_high(high);
-  ohlc_tick->set_low(low);
-  ohlc_tick->set_close(close);
-  ohlc_tick->set_volume(1000.0f);
-}
-
-// Adds OHLC tick to the history period_sec away from the last OHLC tick.
-void AddOhlcTickWithPeriod(float open, float high, float low, float close,
-                           int period_sec, OhlcHistory* ohlc_history) {
-  int timestamp_sec = 1483228800;  // 2017-01-01
-  if (!ohlc_history->empty()) {
-    ASSERT_FLOAT_EQ(ohlc_history->back().close(), open);
-    timestamp_sec = ohlc_history->back().timestamp_sec() + period_sec;
-  }
-  AddOhlcTick(open, high, low, close, ohlc_history);
-  ohlc_history->back().set_timestamp_sec(timestamp_sec);
-}
-
-void Add8HourOhlcTick(float open, float high, float low, float close,
-                      OhlcHistory* ohlc_history) {
-  AddOhlcTickWithPeriod(open, high, low, close, kSecondsPer8Hours,
-                        ohlc_history);
-}
-}  // namespace
+using ::trader::testing::PrepareExampleOhlcHistory;
 
 TEST(SimpleMovingAverageTest, Get3SMAWhenAdding8HourOhlcTicks) {
   OhlcHistory ohlc_history;
-  Add8HourOhlcTick(100, 150, 80, 120, &ohlc_history);  // 2017-01-01
-  Add8HourOhlcTick(120, 180, 100, 150, &ohlc_history);
-  Add8HourOhlcTick(150, 250, 100, 140, &ohlc_history);
-  Add8HourOhlcTick(140, 150, 80, 100, &ohlc_history);  // 2017-01-02
-  Add8HourOhlcTick(100, 120, 20, 50, &ohlc_history);
-  Add8HourOhlcTick(50, 100, 40, 80, &ohlc_history);
-  Add8HourOhlcTick(80, 180, 50, 150, &ohlc_history);  // 2017-01-03
-  Add8HourOhlcTick(150, 250, 120, 240, &ohlc_history);
-  Add8HourOhlcTick(240, 450, 220, 400, &ohlc_history);
-  Add8HourOhlcTick(400, 450, 250, 300, &ohlc_history);  // 2017-01-04
-  Add8HourOhlcTick(300, 700, 220, 650, &ohlc_history);
-  Add8HourOhlcTick(650, 800, 600, 750, &ohlc_history);  // 2017-01-06 08:00
-  ohlc_history.back().set_timestamp_sec(ohlc_history.back().timestamp_sec() +
-                                        1 * kSecondsPerDay +
-                                        2 * kSecondsPer8Hours);
+  PrepareExampleOhlcHistory(&ohlc_history);
 
   SimpleMovingAverage simple_moving_average(/*num_ohlc_ticks=*/3,
                                             /*period_size_sec=*/kSecondsPerDay);
---
+++ indicators/test_util.h
@@ -0,0 +1,31 @@
+// Copyright © 2020 Peter Cerno. All rights reserved.
+
+#ifndef INDICATORS_TEST_UTIL_H
+#define INDICATORS_TEST_UTIL_H
+
+#include "gtest/gtest.h"
+#include "lib/trader_base.h"
+
+namespace trader {
+namespace testing {
+
+// Prepares the following OHLC history:
+// O: 100  H: 150  L:  80  C: 120  V: 1000  T: 2017-01-01 00:00
+// O: 120  H: 180  L: 100  C: 150  V: 1000  T: 2017-01-01 08:00
+// O: 150  H: 250  L: 100  C: 140  V: 1000  T: 2017-01-01 16:00
+// O: 140  H: 150  L:  80  C: 100  V: 1000  T: 2017-01-02 00:00 (+1 Day)
+// O: 100  H: 120  L:  20  C:  50  V: 1000  T: 2017-01-02 08:00
+// O:  50  H: 100  L:  40  C:  80  V: 1000  T: 2017-01-02 16:00
+// O:  80  H: 180  L:  50  C: 150  V: 1000  T: 2017-01-03 00:00 (+1 Day)
+// O: 150  H: 250  L: 120  C: 240  V: 1000  T: 2017-01-03 08:00
+// O: 240  H: 450  L: 220  C: 400  V: 1000  T: 2017-01-03 16:00
+// O: 400  H: 450  L: 250  C: 300  V: 1000  T: 2017-01-04 00:00 (+1 Day)
+// O: 300  H: 700  L: 220  C: 650  V: 1000  T: 2017-01-04 08:00
+// --- Gap --
+// O: 650  H: 800  L: 600  C: 750  V: 1000  T: 2017-01-06 08:00
+void PrepareExampleOhlcHistory(OhlcHistory* ohlc_history);
+
+}  // namespace testing
+}  // namespace trader
+
+#endif  // INDICATORS_TEST_UTIL_H
